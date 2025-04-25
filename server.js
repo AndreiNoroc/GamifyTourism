9eb563315b09ade735c630172e4e74cc6507788d
AndreiNoroc <noroc_andrei@yahoo.com>
2025-04-25 20:18:21 +0300

    change port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
-const PORT = 3000;
+const PORT = 8080;
 
 // Middleware
 app.use(cors());
